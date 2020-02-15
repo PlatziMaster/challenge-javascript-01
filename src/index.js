4008240247886d1fccf91c4f91c7085d86a3ff3d
Jorgelisapa <jorgelisapa@gmail.com>
2020-02-14 19:42:02 -0500

    Functions to find the area of a triangle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+/* let constant = 2
 const triangleArea = (base, height) => {
+    return(base * height) / constant
+}; */
 
-};
+let constant = 2
+const triangleArea = (base, height) => base * height / constant
 
 module.exports = triangleArea;
