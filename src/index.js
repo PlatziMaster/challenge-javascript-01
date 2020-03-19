3573fa2ac70b43302af699225af9e60ddbd45319
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-03-18 19:38:47 -0600

    correccion, reto cumplido

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const triangleArea = (base, height) => {
     let area = base * height / 2
-    let result = `Èl area de su triaángulo es: ${area}`
-    return result
+    return area
 };
 
-console.log(triangleArea(3, 98));
+console.log(triangleArea(3, 12));
 
 module.exports = triangleArea;
