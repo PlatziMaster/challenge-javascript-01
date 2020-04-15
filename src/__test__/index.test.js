84286ed12f500187265cd9b264c5c2fa88ae93a2
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-15 18:06:11 -0500

    add valid number

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -4,4 +4,16 @@ describe('Test Triangle Area Function', () => {
   test('Calculate Area', () => {
     expect(triangleArea(7, 4)).toEqual(14);
   });
+
+  test('Calculate Area string number', () => {
+    expect(triangleArea('7', '4')).toEqual(14);
+  });
+
+  test('Calculate Area negative', () => {
+    expect(triangleArea(-2,-6)).toEqual(false);
+  });
+
+  test('Calculate Area string', () => {
+    expect(triangleArea('qweqwe', 'qweqwe')).toEqual(false);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+const isNumber = require('is-number');
+
 const triangleArea = (base, height) => {
 
+    if(!validNumber(base)){
+        return false;
+    }
+
+    if(!validNumber(height)){
+        return false;
+    }
+    
+    return ((base * height) / 2);
 };
+
+function validNumber(number){
+    if(!isNumber(number)){
+        return false;
+    }else if(number < 1){
+        return false;
+    }
+
+    return true;
+}
 
 module.exports = triangleArea;
