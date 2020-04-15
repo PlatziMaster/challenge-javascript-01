7b48c7b430fcfc6de8d4cf2cb89abdde778fb5f0
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-15 18:09:50 -0500

    new test

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -16,4 +16,12 @@ describe('Test Triangle Area Function', () => {
   test('Calculate Area string', () => {
     expect(triangleArea('qweqwe', 'qweqwe')).toEqual(false);
   });
+
+  test('Calculate Area Null', () => {
+    expect(triangleArea(null, '1')).toEqual(false);
+  });
+
+  test('Calculate Area empty', () => {
+    expect(triangleArea(1, '')).toEqual(false);
+  });
 });
