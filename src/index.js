082721084edaf994633b783446ce98bc3ba25779
lenninIbarraGonzalez <soporte@allnexus.com.co>
2020-05-11 11:05:13 -0500

    resolviendo reto 1 area de un triangulo en javascript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const triangleArea = (base, height) => {
-
+  const divider = 2;
+  let area = (base * height) / divider;
+  return area;
 };
+
+triangleArea(4, 8);
 
 module.exports = triangleArea;
