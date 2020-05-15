b33f27f61e4fceb93810d13a99c14ca66b214257
Sergio Leal <checholeal7@gmail.com>
2020-05-15 10:54:38 -0500

    Fibonacci sequence

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const triangleArea = (base, height) => {
  
-    return base * height /2 
+    return base * height / 2 
  
 };
 
-triangleArea(8,5)
 
-console.log(triangleArea)
+
 
 module.exports = triangleArea;
