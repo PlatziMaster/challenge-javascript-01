fb26e1c2f8b990de0a96f4250c41c58a2de0e80e
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-17 20:33:15 -0500

    reto terminado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const triangleArea = (base, height) => {
+// const triangleArea = (base, height) => {
+//     var multiplication = base * height;
+//     var result = multiplication / 2;
+//     return result;
+// };
 
+const triangleArea = (base, height) => {
+    var result = (base * height)/2;
+    console.log(`El area de un taringulo con ${base}m de base y ${height}m de altura, es igual a ${result}m`);
 };
+
+triangleArea(8, 4);
 
 module.exports = triangleArea;
