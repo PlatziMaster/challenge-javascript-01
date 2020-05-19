8feb6fc15ca3b8e21846f32c81957e47a0faa123
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-19 12:21:43 -0500

    Se eliminan lineas de codigo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-// const triangleArea = (base, height) => {
-//     var multiplication = base * height;
-//     var result = multiplication / 2;
-//     return result;
-// };
-
 const triangleArea = (base, height) => {
-    var result = (base * height)/2;
-    console.log(`El area de un taringulo con ${base}m de base y ${height}m de altura, es igual a ${result}m`);
+    var result = (base * height) / 2;
 };
-
-triangleArea(8, 4);
-
 module.exports = triangleArea;
