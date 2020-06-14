e20e7750cb989e2c820fc0f5c21473558035d710
soyraffaello <tecnoportuaria@gmail.com>
2020-06-14 17:42:25 -0500

    reto cumplido

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -2,6 +2,6 @@ const triangleArea = require('../index');
 
 describe('Test Triangle Area Function', () => {
   test('Calculate Area', () => {
-    expect(triangleArea(7, 4)).toEqual(14);
+    expect(triangleArea(7, 4)).toEqual(14)
   });
 });
