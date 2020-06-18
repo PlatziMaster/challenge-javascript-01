3f74f7974850be627ecc6f5c8355cd67198b71fa
David Mosquera <mosqueradavid@outlook.es>
2020-06-17 22:49:05 -0500

    Better late than never

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const triangleArea = (base, height) => {
+  const triangle_area = (base * height) / 2
+  return triangle_area
+}
 
-};
+console.log(triangleArea(20, 50))
 
 module.exports = triangleArea;
