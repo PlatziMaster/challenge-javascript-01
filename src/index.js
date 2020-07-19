abacf1ddf1de75a32ff93b81d777ce8fc0447556
JhonnierGS <gs.170590@gmail.com>
2020-07-18 20:19:24 -0500

    Agrego función para Area del triangulo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const triangleArea = (base, height) => {
 
-};
 
-module.exports = triangleArea;
+ function triangulo(resultado)
+  {
+    var base = prompt("ingrese la base");
+    var height = prompt("ingrese el area");
+    var area =  base * height / 2;
+    console.log(`El area del triangulo es ${area}`);
+  }
+
+
+  triangulo();
+
+
+
+
+
+
+
+//const triangleArea = (base, height) => {
+//      return area =  base * altura / 2;
+//  };
+
+ //module.exports = triangleArea;
