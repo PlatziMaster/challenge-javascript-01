4ce03ec60f43b6c5d727f3cddb0332fc6e886aea
JhonnierGS <gs.170590@gmail.com>
2020-07-21 10:58:04 -0500

    agregue dos lineas nuevas al reto

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 
 
- function triangulo(resultado)
-  {
-    var base = prompt("ingrese la base");
-    var height = prompt("ingrese el area");
-    var area =  base * height / 2;
-    console.log(`El area del triangulo es ${area}`);
-  }
+
+	 const triangleArea = (base, height) => {
+	 const area = (base * height)/2;
+	 console.log("El area del triangulo es: " + area);	 
+         return area   };
 
 
-  triangulo();
-
-
-
-
-
-
-
-//const triangleArea = (base, height) => {
-//      return area =  base * altura / 2;
-//  };
-
- //module.exports = triangleArea;
+ 	module.exports = triangleArea;
