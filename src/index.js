9e2b9e94cc40a21e255dedbeb239918544c84d7e
Pitupan <rojas.fredic6@gmail.com>
2020-07-22 11:20:43 -0500

    Realizacion del ejercicio de challenge-1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const triangleArea = (base, height) => {
-
+	let area = (base * height)/2;
+	console.log(`El area del triangulo es ${area}`);
 };
+
+triangleArea(5,3)
 
 module.exports = triangleArea;
