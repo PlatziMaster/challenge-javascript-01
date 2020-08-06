92dd05d98694b526fc79b630b0df2dfa14014bf8
Walter Salas <soy@waltersalas.com>
2020-08-06 16:48:23 -0500

    Challenge solved!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const triangleArea = (base, height) => {
 
+    let area = ((base*height) / 2)
+    return area;
+
 };
+
+result = triangleArea(2, 4);
+console.log(result);
 
 module.exports = triangleArea;
