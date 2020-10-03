e37c195f1a51c72c5516dafdb3857a4f5312a450
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-03 17:55:42 -0500

    Construction of triangle function

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const triangleArea = require('../index');
 
 describe('Test Triangle Area Function', () => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const triangleArea = (base, height) => {
+'use strict'
 
+function area(a) {
+  const divided = 2
+  return function base(b) {
+    return (a * b) / divided
+  }
+}
+
+
+const triangleArea = (base, height) => {
+  const baseTriangule = area(base)
+  return baseTriangule(height)
 };
 
 module.exports = triangleArea;
